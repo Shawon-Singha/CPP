520f2cdd3780ec3bad0f5daba15f735cba71139e
Your Name <shawonsingha10@gmail.com>
2023-05-04 23:18:23 +0600

    update

---
+++ 2Variable/Float/Main1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1 = 10.4, num2 = 20.4;
+
+    double num3 = 30.546254;
+
+    
+
+    cout << num1 << endl
+         << num2 << endl
+         << num3 << endl;
+
+    
+}
