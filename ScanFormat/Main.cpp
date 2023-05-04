4619b32600e7e59b6eafea2acfebcfe3bd9b58b1
Your Name <shawonsingha10@gmail.com>
2023-05-04 23:29:44 +0600

    update

---
+++ ScanFormat/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter ethe number : ";
+
+    cin >> num;
+
+    cout << "The number is :" << num;
+
+    return 0;
+}
