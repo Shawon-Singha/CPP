dd12deeda4da7f67283fd91e98aa232fccac0585
Your Name <shawonsingha10@gmail.com>
2023-05-04 23:37:25 +0600

    update

--- ScanFormat/Main1.cpp
+++ ScanFormat/Main1.cpp
@@ -5,11 +5,21 @@ int mian()
 {
     char ch;
 
+    char ch1[15];
+
     cout << "Enter your char :";
 
     cin >> ch;
 
+ 
+    cout << "Enter your char1 :";
+
+    cin >> ch1;
+
+
     cout << "Your char is : " << ch;
+    cout << "Your char1 is : " << ch1;
+  
 
     return 0;
 }
