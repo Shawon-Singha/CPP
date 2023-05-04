9c6cc3f7bd4570870ca6dc701d335c7cab7a454b
Your Name <shawonsingha10@gmail.com>
2023-05-04 23:25:15 +0600

    update

---
+++ 2Variable/Character & String/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch = 'S';
+    char name[14] = "Shawon Singha";
+
+    //string name = "Shawon Singha";
+
+    cout << ch << endl
+         << name << endl;
+
+    return 0;
+}
