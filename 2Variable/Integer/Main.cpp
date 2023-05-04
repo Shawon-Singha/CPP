02cce84d8bdf94dc91acac4813d5ec2ff62bc30a
Your Name <shawonsingha10@gmail.com>
2023-05-04 23:10:41 +0600

    update

---
+++ 2Variable/Integer/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    // variable declare.
+    int num1, num2;
+
+    // variable initialization
+    num1 = 10;
+    num2 = 20;
+
+    cout << num1 << endl
+         << num2 << endl;
+}
