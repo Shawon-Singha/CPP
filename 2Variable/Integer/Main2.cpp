d6b45de72c35664dc6228c79fb6a4583815967e8
Your Name <shawonsingha10@gmail.com>
2023-05-14 22:30:29 +0600

    update

---
+++ 2Variable/Integer/Main2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+
+    cout << "Enter the value :";
+
+    cin >> num1   >> num2;
+
+    /* cin >> num1
+            >> num2
+
+        both are correct.
+    */
+
+  // cout << "First number is :" << num1 << endl << "Second number is :" << num2 << endl;
+
+  cout << "First number is :" << num1 << endl 
+         << "Second number is :" << num2 << endl;
+
+         // both output formate is correct.
+}
