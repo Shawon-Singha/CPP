5acbc4559241e27db56687b85188a233b079e411
Your Name <shawonsingha10@gmail.com>
2023-05-15 00:17:50 +0600

    update

--- 4Arithmetic_Operator/Main.cpp
+++ 4Arithmetic_Operator/Main.cpp
@@ -13,11 +13,31 @@ int main()
 
     a = b + 7;
 
+
     /*here "a,b" are operand , "=,+" are operator , 7 is constant.
      total equation is a express.
     */
 
-    cout << " Value of a is :" << a;
+    cout << " Value of a is :" << a << endl;
+
+
+   /*Arithmetic operator (+,-,*,/,%)*/;
+
+   int add = a + b;
+   int subtraction  = a - b;
+   int Multi = a * b;
+   int division = (float) a / b; //type casting.
+   int remainder  = a % b;
+
+   cout << "balue of Add :" << add << endl;
+   cout << "balue of Subtraction :" << subtraction << endl;
+   cout << "balue of Multi :" << Multi << endl;
+   cout << "balue of Division :" << division << endl;
+   cout << "balue of Remainder :" << remainder << endl;
+   
+    
+
+   
 
     getch();
 }
