c19d50fe90a673ca861cb956149d6ee37ed61de2
Your Name <shawonsingha10@gmail.com>
2023-05-29 22:42:29 +0600

    update

---
+++ 8Loop/2While/Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+
+    while (i<=5)
+    {
+        cout << "Hey Legend"<< endl;   
+        i++; 
+    }
+
+    return 0;
+    
+}
