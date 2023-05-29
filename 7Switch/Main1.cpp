0f63438cf838c410bf78c8a5eaffef84f1c2b946
Your Name <shawonsingha10@gmail.com>
2023-05-29 18:51:55 +0600

    update

---
+++ 7Switch/Main1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout << "Enter num: ";
+    cin >> num;
+
+    switch (num)
+    {
+    case 1:
+        cout << "This value is one";
+        break;
+    case 2:
+        cout << "This value is Two";
+        break;
+    case 3:
+        cout << "This value is Three";
+        break;
+    case 4:
+        cout << "This value is Four";
+        break;
+    case 5:
+        cout << "This value is Five";
+        break;
+    
+    default:
+         cout << "Invalid";
+        break;
+    }
+
+    return 0;
+}
