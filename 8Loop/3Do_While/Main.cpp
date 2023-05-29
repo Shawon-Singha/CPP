121f101812a8f705d599a9b701e53f828d526c51
Your Name <shawonsingha10@gmail.com>
2023-05-29 22:45:09 +0600

    update

---
+++ 8Loop/3Do_While/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i = 1;
+
+    do
+    {
+        cout << "Do you practice others two?" << endl ;
+
+        i++;
+    } while (i<=5);
+    
+
+    return 0;
+}
