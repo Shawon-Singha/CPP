2b4dfc0f85522bdf59c28b5d57161e8d8e19022e
Your Name <shawonsingha10@gmail.com>
2023-06-02 19:09:57 +0600

    update

---
+++ 10Array/2D/Main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[2][3] = {{10,20,30},{40,50,60}};   
+                
+                 
+                
+
+
+     cout <<  a[0][0] << " ";
+     cout <<  a[0][1] << " " ;
+     cout <<  a[0][2] << endl;
+     cout <<  a[1][0] << " " ;
+     cout <<  a[1][1] << " ";
+     cout <<  a[1][2] << endl;
+
+     return 0;
+
+
+}
