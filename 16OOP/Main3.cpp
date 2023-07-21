076bb27e4476eee9c9022ff21b55f1ea7cccbdb4
Shawon Singha <shawonsingha10@gmail.com>
2023-07-21 09:49:11 +0600

    update

--- 16OOP/Main3.cpp
+++ 16OOP/Main3.cpp
@@ -8,7 +8,8 @@ class Student{
     int id;
     double gpa;
 
-    Student(int x, double y){
+    Student(int x, double y)  //parametrized constructor...
+    {
         id = x;
         gpa = y;
     }
@@ -17,9 +18,17 @@ class Student{
         cout << " Id is : " << id << endl
              << "GPA is : " << gpa << endl;
     }
+
+    Student() //default constructor
+    {
+        cout << "It is a default constructor." << endl;
+    }
 };
 
 int main(){
+
+    Student deFault; // for default constructor
+
     Student shawon(5290,3.76);
     shawon.display();
 }
